66fe6e8416bc2f8573861a9469fc1204f68dd1d6
321-Closet <thefandomchild7@gmail.com>
2025-05-13 13:37:36 -0400

    Update build.py

--- build.py
+++ build.py
@@ -16,3 +16,18 @@ for file in os.listdir(src_directory): # list out files
         src = src_directory
         destination = destination_directory
         shutil.move(src+'/'+file, destination) # move the file to the destination
+        # edit file src for working directory
+        with open(destination+'/'+file, "r+") as file_data:
+            content = file_data.read()
+            tokens = content.split("\n")
+            new_dir = destination_directory.replace("c", "C", 1)
+            new_dir = new_dir.replace("\\", "/")
+            file_path = str.split(file_data.name, "/")
+            file_name = file_path[len(file_path)-1].removesuffix(".ts")
+            tokens.remove('declare module "C:/berry/src/'+file_name+'"'+";")
+            tokens[len(tokens)-1] = 'declare module "'+new_dir+"/"+file_name+'"'
+            new_src = "\n".join(tokens)
+            print(new_src)
+            file_data.seek(0)
+            file_data.write(new_src)
+            file_data.truncate()
